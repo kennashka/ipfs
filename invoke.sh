19359df8c05a1eb1af2c365f6f32c55db8ffb23c
Kennashka DeSilva <kennashka@yahoo.com>
2022-02-11 23:32:45 -0500

    Create invoke.sh
    installation

---
+++ invoke.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+########################################################################################################
+#Author: Kennashka DeSilva
+#mail@kennashka.com
+#Feb 11, 2022
+########################################################################################################
+
+
+# Install ipfs
+
+curl -O https://dist.ipfs.io/go-ipfs/v0.11.0/go-ipfs_v0.11.0_darwin-amd64.tar.gz
+
+tar -xvzf go-ipfs_v0.11.0_darwin-amd64.tar.gz
+
+########################################################################################################
+
+# > x go-ipfs/install.sh
+# > x go-ipfs/ipfs
+# > x go-ipfs/LICENSE
+# > x go-ipfs/LICENSE-APACHE
+# > x go-ipfs/LICENSE-MIT
+# > x go-ipfs/README.md
+
+
+cd go-ipfs
+bash install.sh
+
+# > Moved ./ipfs to /usr/local/bin
+
+ipfs --version
+
+# > ipfs version 0.11.0
